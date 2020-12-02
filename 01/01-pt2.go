2e6e01e5d5ee83f9ef9b00f9eb6b53fe38d103c9
Jeremy O'Brien <neutral@fastmail.com>
2020-12-02 13:03:07 -0500

    day 1 complete

---
+++ 01/01-pt2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var arr []int
+
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		arr = append(arr, num)
+	}
+	for i := range arr {
+		for j := i + 1; j < len(arr); j++ {
+			for k := j + 1; k < len(arr); k++ {
+				if arr[i]+arr[j]+arr[k] == 2020 {
+					fmt.Println(arr[i] * arr[j] * arr[k])
+				}
+			}
+		}
+	}
+}
